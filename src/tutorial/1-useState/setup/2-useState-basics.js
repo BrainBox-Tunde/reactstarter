a837a905056591b06d79231782c97d2ce17d73d5
Tunde BrainBox <realtundeonline@gmail.com>
2022-07-19 12:36:50 +0100

    Completed UseStateBasics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Hello, Tunde");
+  const handleClick = () => {
+    if (text === "Hello, Tunde"){
+      setText("Welcome, Tunde")
+    } else{
+      setText("Hello, Tunde")
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>Click Here</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
